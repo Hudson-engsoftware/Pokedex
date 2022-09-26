941b86e4daa7f55da808e0244c1a3c77268370e6
hudson <hudsonengesoftware@outlook.com>
2022-09-26 20:26:12 -0300

    pokedex

---
+++ JS/script.js
@@ -0,0 +1,38 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+
+let searchPokemon = 1;
+
+const fetchPokemon= async (Pokemon)=>{
+
+    const  APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon}`);
+    const data = await APIResponse.json();
+
+    return data;
+
+}
+
+const renderPokemon = async (pokemon) =>{
+
+const data = await fetchPokemon (pokemon);
+
+
+pokemonName.innerHTML= data.name;
+pokemonNumber.innerHTML= data.id;
+pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front-default'];
+
+
+}
+
+form.addEventListener('submit' ,(event) => {
+
+event.preventDefault();
+
+renderPokemon(input.value.toLowerCase());
+input.value = '';
+
+});
